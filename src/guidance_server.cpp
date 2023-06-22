c6d1cd6a8ee8b4cc3abe3d4e6f1f7e2a831ac0ec
Diego Martínez Baselga <diegomartinez@unizar.es>
2023-06-22 11:48:46 +0200

    training worked

--- src/guidance_server.cpp
+++ src/guidance_server.cpp
@@ -281,86 +281,93 @@ public:
 
     bool guidances_Hcost_callback(guidance_planner::guidances_Hcost::Request &req, guidance_planner::guidances_Hcost::Response &res)
     {
-        guidance.Reset();
-        guidance.SetStart(Eigen::Vector2d(req.x, req.y), req.oriantation, req.v); // Position, yaw angle, velocity magnitude
-        std::vector<Goal> goals;
-        for (size_t i = 0; i < req.goals_x.size(); i++)
-        {
-            goals.emplace_back(Eigen::Vector2d(req.goals_x[i], req.goals_y[i]), 1.);
-        }
-        guidance.SetGoals(goals);
-        std::vector<GuidancePlanner::Obstacle> obstacles_vec;
-        for (size_t i = 0; i < req.obstacles.size(); i++)
-        {
-            std::vector<Eigen::Vector2d> obs_pos;
-            PRM_LOG("Request obstacle len: " << req.obstacles[i].pos_x.size());
-            for (size_t j = 0; j < req.obstacles[i].pos_x.size(); j++)
+        try{
+            guidance.Reset();
+            guidance.SetStart(Eigen::Vector2d(req.x, req.y), req.oriantation, req.v); // Position, yaw angle, velocity magnitude
+            std::vector<Goal> goals;
+            for (size_t i = 0; i < req.goals_x.size(); i++)
             {
-                obs_pos.emplace_back(req.obstacles[i].pos_x[j], req.obstacles[i].pos_y[j]);
+                goals.emplace_back(Eigen::Vector2d(req.goals_x[i], req.goals_y[i]), 1.);
             }
-            obstacles_vec.push_back(GuidancePlanner::Obstacle(req.obstacles[i].id, obs_pos, req.obstacles[i].radius));
-        }
-        std::vector<RosTools::Halfspace> static_obstacles;
-        for (size_t i = 0; i < req.static_x.size(); i++)
-        {
-            static_obstacles.emplace_back(Eigen::Vector2d(req.static_x[i], req.static_y[i]), req.static_n[i]);
-        }
-        guidance.LoadObstacles(obstacles_vec, static_obstacles);
-        // Update (i.e., compute) the guidance trajectories
-        guidance.Update();
-        // Show some results:
-        bool success = guidance.Succeeded();
-        res.success = success;
-        if (success)
-        {
-            std::vector<GuidancePlanner::Node *> truth_vec_node;
-            for (size_t i_node = 0; i_node < req.truth.x.size(); i_node++)
+            guidance.SetGoals(goals);
+            std::vector<GuidancePlanner::Obstacle> obstacles_vec;
+            for (size_t i = 0; i < req.obstacles.size(); i++)
             {
-                SpaceTimePoint point(req.truth.x[i_node], req.truth.y[i_node], i_node);
-                truth_vec_node.emplace_back(new Node(i_node, point, NodeType::NONE));
-            }
-            GeometricPath truth_path(truth_vec_node);
-            ROS_INFO_STREAM("Guidance planner found: " << guidance.NumberOfGuidanceTrajectories() << " trajectories");
-            // for (int i = 0; i < req.n_trajectories; i++){
-            for (int i = 0; i < guidance.NumberOfGuidanceTrajectories(); i++)
-            {
-                // CubicSpline3D& guidance_spline = guidance.GetGuidanceTrajectory(i%guidance.NumberOfGuidanceTrajectories());
-                CubicSpline3D &guidance_spline = guidance.GetGuidanceTrajectory(i);
-                RosTools::CubicSpline2D<tk::spline> guidance_trajectory = guidance_spline.GetTrajectory(); // Retrieves the trajectory: t -> (x, y))
-                std::vector<double> x_traj, y_traj;
-                for (double t = 0; t <= Config::N * Config::DT; t += Config::DT)
+                std::vector<Eigen::Vector2d> obs_pos;
+                PRM_LOG("Request obstacle len: " << req.obstacles[i].pos_x.size());
+                for (size_t j = 0; j < req.obstacles[i].pos_x.size(); j++)
                 {
-                    Eigen::Vector2d pos = guidance_trajectory.GetPoint(t);
-                    x_traj.emplace_back(pos.x());
-                    y_traj.emplace_back(pos.y());
-                    // ROS_INFO_STREAM("\t[t = " << t << "]: (" << pos(0) << ", " << pos(1) << ")");
+                    obs_pos.emplace_back(req.obstacles[i].pos_x[j], req.obstacles[i].pos_y[j]);
                 }
-                guidance_planner::TrajectoryMSG traj;
-                traj.x = x_traj;
-                traj.y = y_traj;
-                res.trajectories.emplace_back(traj);
-                res.costs.emplace_back(guidance.GetHomotopicCost(i, truth_path));
-                guidance_planner::LeftHMSG h_signature_msg;
-                std::vector<bool> right = guidance.LeftPassingH(i);
-                for(int i_obs = 0; i_obs < (int)right.size(); i_obs++){
-                    h_signature_msg.left_passing.push_back((double)right[i_obs]);
-                }
-                res.h_signature.emplace_back(h_signature_msg);
-                // RosTools::CubicSpline2D<tk::spline> guidance_path = guidance_spline.GetPath(); // Retrieves the path: s -> (x, y)
+                obstacles_vec.push_back(GuidancePlanner::Obstacle(req.obstacles[i].id, obs_pos, req.obstacles[i].radius));
             }
-            // guidance.Visualize();
-            ros::spinOnce();
-            // CubicSpline3D& guidance_spline = guidance.GetGuidanceTrajectory(0);
+            std::vector<RosTools::Halfspace> static_obstacles;
+            for (size_t i = 0; i < req.static_x.size(); i++)
+            {
+                static_obstacles.emplace_back(Eigen::Vector2d(req.static_x[i], req.static_y[i]), req.static_n[i]);
+            }
+            guidance.LoadObstacles(obstacles_vec, static_obstacles);
+            // Update (i.e., compute) the guidance trajectories
+            guidance.Update();
+            // Show some results:
+            bool success = guidance.Succeeded();
+            res.success = success;
+            if (success)
+            {
+                std::vector<GuidancePlanner::Node *> truth_vec_node;
+                for (size_t i_node = 0; i_node < req.truth.x.size(); i_node++)
+                {
+                    SpaceTimePoint point(req.truth.x[i_node], req.truth.y[i_node], i_node);
+                    truth_vec_node.emplace_back(new Node(i_node, point, NodeType::NONE));
+                }
+                GeometricPath truth_path(truth_vec_node);
+                ROS_INFO_STREAM("Guidance planner found: " << guidance.NumberOfGuidanceTrajectories() << " trajectories");
+                // for (int i = 0; i < req.n_trajectories; i++){
+                for (int i = 0; i < guidance.NumberOfGuidanceTrajectories(); i++)
+                {
+                    // CubicSpline3D& guidance_spline = guidance.GetGuidanceTrajectory(i%guidance.NumberOfGuidanceTrajectories());
+                    CubicSpline3D &guidance_spline = guidance.GetGuidanceTrajectory(i);
+                    RosTools::CubicSpline2D<tk::spline> guidance_trajectory = guidance_spline.GetTrajectory(); // Retrieves the trajectory: t -> (x, y))
+                    std::vector<double> x_traj, y_traj;
+                    for (double t = 0; t <= Config::N * Config::DT; t += Config::DT)
+                    {
+                        Eigen::Vector2d pos = guidance_trajectory.GetPoint(t);
+                        x_traj.emplace_back(pos.x());
+                        y_traj.emplace_back(pos.y());
+                        // ROS_INFO_STREAM("\t[t = " << t << "]: (" << pos(0) << ", " << pos(1) << ")");
+                    }
+                    guidance_planner::TrajectoryMSG traj;
+                    traj.x = x_traj;
+                    traj.y = y_traj;
+                    res.trajectories.emplace_back(traj);
+                    res.costs.emplace_back(guidance.GetHomotopicCost(i, truth_path));
+                    guidance_planner::LeftHMSG h_signature_msg;
+                    std::vector<bool> right = guidance.LeftPassingH(i);
+                    for(int i_obs = 0; i_obs < (int)right.size(); i_obs++){
+                        h_signature_msg.left_passing.push_back((double)right[i_obs]);
+                    }
+                    res.h_signature.emplace_back(h_signature_msg);
+                    // RosTools::CubicSpline2D<tk::spline> guidance_path = guidance_spline.GetPath(); // Retrieves the path: s -> (x, y)
+                }
+                // guidance.Visualize();
+                ros::spinOnce();
+                // CubicSpline3D& guidance_spline = guidance.GetGuidanceTrajectory(0);
 
-            /** @note If you decide on a used path, you can provide this feedback to the guidance planner and it will remember which path is best */
-            // int used_trajectory_id = guidance.GetUsedTrajectory()
-            // guidance.SetUsedTrajectory(int spline_id);
+                /** @note If you decide on a used path, you can provide this feedback to the guidance planner and it will remember which path is best */
+                // int used_trajectory_id = guidance.GetUsedTrajectory()
+                // guidance.SetUsedTrajectory(int spline_id);
+            }
+            else
+            {
+                ROS_WARN("\tGuidance planner found no trajectories that reach any of the goals!");
+            }
+            return true;
         }
-        else
-        {
+        catch (IntegrationException ie){
+            res.success = false;
             ROS_WARN("\tGuidance planner found no trajectories that reach any of the goals!");
+            return true;
         }
-        return true;
     }
 };
 
